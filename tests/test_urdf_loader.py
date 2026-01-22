b4631c105cc46a4f7b208f9fe8af01836d07b986
Peter Detzner <peter.detzner@w-hs.de>
2026-01-22 12:40:15 +0100

    Simple test to verify pyroki and some of its dependencies are properly installed

---
+++ tests/test_urdf_loader.py
@@ -0,0 +1,26 @@
+"""Basic IK
+
+Simplest Inverse Kinematics Example using PyRoki.
+"""
+
+import time
+
+import numpy as np
+import pyroki as pk
+import viser
+from robot_descriptions.loaders.yourdfpy import load_robot_description
+from viser.extras import ViserUrdf
+
+ 
+# Simple test to verify pyroki and some of its dependencies are properly installed
+def test_urdf_loader():
+    """Main function for basic IK."""
+
+    urdf = load_robot_description("panda_description")
+    target_link_name = "panda_hand"
+
+    # Create robot.
+    robot = pk.Robot.from_urdf(urdf)
+
+if __name__ == "__main__":
+    test_urdf_loader()
